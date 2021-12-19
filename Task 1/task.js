bc52df1b99b06e63664abadaaa0053b1438a4021
vavsn <41681585+vavsn@users.noreply.github.com>
2021-12-19 22:19:39 +0700

    Lesson 1. Homework. Start leaning JavaScript.

---
+++ Task 1/task.js
@@ -0,0 +1,5 @@
+'use script'
+let tCelsius =parseInt(prompt("Введите число:", 0));
+tFarenheit = (9 / 5) * tCelsius + 32
+alert("Температура в градусах Цельсия = " + tCelsius + "\n" + "Температура в градусах Фаренгейта = " + tFarenheit)
+	
---
+++ Task 2/task.js
@@ -0,0 +1,6 @@
+'use script'
+let admin, name 
+name =prompt("Введите имя: ")
+admin = name
+alert("Пользоательь ввел имя = " + name + "\n" + "Переменная admin = " + admin)
+	
